ed966e3bfc2989154e2c3222977b730e536f24b9
shari-ar <156581474+shari-ar@users.noreply.github.com>
2024-01-15 13:09:33 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+const fs = require('fs').promises;
+const path = require('path');
+const jhtml = require('./frontobjects-lib/jhtml');
+
+
+async function getAllFiles(directoryPath, extensions) {
+
+  try {
+    const allFiles = [];
+    const files = await fs.readdir(directoryPath);
+
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file);
+      const fileStat = await fs.stat(filePath);
+
+      if (fileStat.isDirectory()) {
+        const subdirectoryFiles = await getAllFiles(filePath, extensions);
+        allFiles.push(...subdirectoryFiles);
+      } else if (extensions.includes(path.extname(filePath))) {
+        allFiles.push(filePath);
+      }
+    }
+
+    return allFiles;
+  } catch (error) {
+    throw new Error(`Error in getAllFiles`, error);
+  }
+
+}
+
+
+async function readJObjectFiles(files) {
+
+  const jobjects = await Promise.all(files.map(async (filePath) => {
+
+    try {
+
+      const code = await fs.readFile(filePath, 'utf8');
+
+      return { path: filePath, code };
+
+    } catch (error) {
+      throw new Error(`Error reading file ${filePath}`, error);
+    }
+
+  }));
+
+  return jobjects.filter(result => result !== null);
+
+}
+
+
+async function render(viewPath, options, callback) {
+  try {
+    const viewContent = await fs.readFile(viewPath, 'utf-8');
+
+    const jhtmlFile = { path: viewPath, code: viewContent };
+
+    if (!options.jobjects) {
+      const jobjectPaths = await getAllFiles(options.viewsPath, ['.jobj']);
+      options.jobjects = await readJObjectFiles(jobjectPaths);
+    }
+
+    const renderedContent = await jhtml.renderAsync(jhtmlFile, options);
+
+    callback(null, renderedContent, options);
+  } catch (err) {
+    callback(err);
+  }
+}
+
+
+module.exports = {
+  render
+};
